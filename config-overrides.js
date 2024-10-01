1634640b8f90a6869834cc52b1f1d81576a8eefe
Dylan Stevens <dylanStevens@student.unsw.edu.au>
2024-10-02 03:18:41 +1000

    correct frontend

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { alias, configPaths } = require('react-app-rewire-alias');
-const path = require('path');
-
-const customConfig = alias(configPaths('./tsconfig.paths.json'));
-
-module.exports = function override(config) {
-  return customConfig(config);
-};
